d95aeacb51e688765f20bfd1a9a1390f82d1b222
Takaaki Hagiwara <hagiwara@andfactory.co.jp>
2021-08-03 18:32:25 +0900

    Zenn初級編2:画面の更新

---
+++ zenn/shokyu02_refresh_screen/lib/main.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  String _type = "偶数";
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+      // if (_counter % 2 == 0) {
+      //   _type = "偶数";
+      // } else {
+      //   _type = "奇数";
+      // }
+      _type = _counter % 2 == 0 ? "偶数" : "奇数";
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            if (_counter % 2 == 0)
+              Text('$_typeだよ', style: TextStyle(fontSize: 20, color: Colors.red),),
+            if (_counter % 2 == 1)
+              Text('$_type', style: TextStyle(fontSize: 20, color: Colors.blue),),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
